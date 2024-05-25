76eeb0cc65c4da98b5542e0d158942c27087301b
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-25 03:34:32 +0300

    feat: wrap redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App.tsx'
+import { StoreProvider } from './redux/provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <StoreProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StoreProvider>
+  // </React.StrictMode>
 )
