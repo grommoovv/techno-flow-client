f2e53afe4b1ec69a59f2cf1778ee3003c5c8aae5
GEORGE GROMOV <gromov.dev13@gmail.com>
2024-05-25 03:47:05 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,11 @@ import { App } from './App.tsx'
 import { StoreProvider } from './redux/provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <StoreProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StoreProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <StoreProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StoreProvider>
+  </React.StrictMode>
 )
